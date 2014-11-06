a26adec89737b9fb475e5ee2a1d5175c01ebdd13
unknown <virtanevakd@D01786969.niaid.nih.gov>
2014-11-06 15:06:22 -0700

    part 2 of the tutorial

--- app.js
+++ app.js
@@ -10,28 +10,31 @@
   
   var gems = [
     {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
-    images: [
-      {
-        full: 'dodecahedron-01-full.jpg',
-        thumb: 'dodecahedron-01-thumb.jpg'
-      },
-      {
-        full: 'dodecahedron-02-full.jpg',
-        thumb: 'dodecahedron-02-thumb.jpg'
-      }
-    ],
-    canPurchase: true,
-    soldOut: false,  // Hide the gem if the file is sold out
+      name: 'Dodecahedron',
+      price: 5.95 * 3,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
+      images: [
+          'images/gem-01.gif',
+          'images/gem-01.gif',
+          'images/gem-01.gif'
+      ],
+      canPurchase: true,
+      soldOut: false  // Hide the gem if the file is sold out
     },
     {
       name: "Pentagonal gem",
       price: 5.95,
       description: ". . .",
+      images: [
+        'images/gem-02.gif',
+        'images/gem-02.gif',
+        'images/gem-02.gif'
+      ],
       canPurchase: false,
-      soldOut: false,
+      soldOut: false
     }
   ];
+
+  console.log(gems);
+
 })();
