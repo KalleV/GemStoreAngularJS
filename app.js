371f7006c0441a34dff54c985d35d8f59b7fc19d
Kalle V <bluesharkies@gmail.com>
2014-11-06 08:35:38 -0700

    Added filter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // 'store' is the application name
 // the '[ ]', contains dependencies
 (function(){  // wraps app into a closure (a lexical scope)
-  var app = angular.module('gemStore', [ ]);
+  var app = angular.module('gemStore', []);
   
   app.controller('StoreController', function(){
     this.products = gems;
@@ -13,6 +13,16 @@
     name: 'Dodecahedron',
     price: 2.95,
     description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
+    images: [
+      {
+        full: 'dodecahedron-01-full.jpg',
+        thumb: 'dodecahedron-01-thumb.jpg'
+      },
+      {
+        full: 'dodecahedron-02-full.jpg',
+        thumb: 'dodecahedron-02-thumb.jpg'
+      }
+    ],
     canPurchase: true,
     soldOut: false,  // Hide the gem if the file is sold out
     },
@@ -21,6 +31,7 @@
       price: 5.95,
       description: ". . .",
       canPurchase: false,
+      soldOut: false,
     }
   ];
 })();
