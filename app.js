f1f22582cc4a44f3fb2e2c9905de1a32df206f16
Kalle V <bluesharkies@gmail.com>
2014-11-06 23:59:25 -0700

    Added user review form

--- app.js
+++ app.js
@@ -7,16 +7,50 @@
   app.controller('StoreController', function(){
     this.products = gems;
   });
-  
+
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(value){
+      this.current = value || 0;
+    };
+    this.hasImages = function(images){
+      return images.length;
+    };
+  });
+
+  app.controller('PanelController', function(){
+    this.tab = 1;
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+    this.panels = null;
+  });
+
   var gems = [
     {
       name: 'Dodecahedron',
       price: 5.95 * 3,
+      shine: 10,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems',
       images: [
           'images/gem-01.gif',
           'images/gem-01.gif',
           'images/gem-01.gif'
+      ],
+      reviews: [
+        {
+        stars: 5,
+        body: "I like this product",
+        author: "joe@thomas.com"
+        },
+        {
+          stars: 1,
+          body: "This product is awful",
+          author: "tim@hater.com"
+        }
       ],
       canPurchase: true,
       soldOut: false  // Hide the gem if the file is sold out
@@ -24,6 +58,7 @@
     {
       name: "Pentagonal gem",
       price: 5.95,
+      shine: 4,
       description: ". . .",
       images: [
         'images/gem-02.gif',
